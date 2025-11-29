d6b2790b1075b91dbfb9a46264f9a975d38c1f90
Aditya <adityarajsrvofficial@gmail.com>
2025-11-29 22:30:30 +0530

    Setting up frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <button className="p-4 border-black border rounded-xl m-5 cursor-pointer bg-red-200">Dashboard</button>
+    </div>
+  )
+}
+
+export default Home
